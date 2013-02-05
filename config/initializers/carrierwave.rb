0c2f786ef8a7afe2ccc16ca93878aa44e553f1c7
Masha Lifshin <mlifshin@gmail.com>
2013-02-05 12:11:59 -0800

    More tweaks for carrierwave and heroku.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,13 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
+  config.root             = Rails.root.join('tmp')
+  config.cache_dir        = 'uploads'
+
+  config.storage          =  :fog
+  config.fog_credentials  = {
     :provider                   => "AWS",
     :aws_access_key_id          => ENV['AWS_ACCESS_KEY_ID'],
     :aws_secret_access_key      => ENV['AWS_SECRET_ACCESS_KEY'],
   }
   
-  config.fog_directory  = ENV['FOG_DIRECTORY']    # for cloudfront
+  config.fog_directory    = ENV['FOG_DIRECTORY']    # for cloudfront
 end
