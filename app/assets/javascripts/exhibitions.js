574c2bade6e5436e4684082a779ef3ac9c4592f8
Masha Lifshin <mlifshin@gmail.com>
2013-02-08 16:26:28 -0800

    Exhibition thumbnail hover UJS.

--- app/assets/javascripts/exhibitions.js
+++ app/assets/javascripts/exhibitions.js
@@ -1,9 +1,18 @@
-
 $(document).ready(function(){
-  $('img[data-mouseover].thumbnail').hover(function() {
-    $(this).data('_mouseover', $(this).attr('src'));
-    $(this).attr('src', $(this).data('mouseover'));
-  },function() {
-    $(this).attr('src', $(this).data('_mouseover'));
-  });
+	$('.thumbnail').hover(function() {
+		var image_data = $(this).data('image');
+		var image_src = image_data['image']['url'];
+		var alt_text = [image_data['artist'], image_data['title'],
+				image_data.year['to_s'], image_data['media'],
+				image_data['dimensions']].join(" - ");
+		var label_dom = $('<div class="label">' + image_data['artist'] +
+						 '<br /><span class="work_title">' + image_data['title'] + '</span>, ' + image_data['year'] +
+						 '<br />' + image_data['media'] +
+						 '<br />' + image_data['dimensions'] +
+						 '</div>');
+
+		$(this).closest('.exhibition').find('.full_size').attr('src', image_src);
+		$(this).closest('.exhibition').find('.full_size').attr('alt', alt_text);
+		$(this).closest('.exhibition').find('.label').replaceWith(label_dom);
+	});
 });
