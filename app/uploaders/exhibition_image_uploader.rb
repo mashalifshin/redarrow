4552d3d53a84edfeeeada49f5b9dfacb8297db57
Masha Lifshin <mlifshin@gmail.com>
2013-02-20 00:22:42 -0800

    Don't create compact versions of exhibition images
    This is not in use.

--- app/uploaders/exhibition_image_uploader.rb
+++ app/uploaders/exhibition_image_uploader.rb
@@ -7,10 +7,6 @@ class ExhibitionImageUploader < ImageUploader
     process :resize_to_fit => [500, 500]
   end
 
-  version :compact do
-    process :resize_to_fit => [200, 200]
-  end
-
   version :thumbnail do
     process :resize_to_fit => [50, 50]
   end
