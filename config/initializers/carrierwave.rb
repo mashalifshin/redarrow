eef73c63a8fa13ac8ca8e151ce4b88bcb8c6bd91
Masha Lifshin <mlifshin@gmail.com>
2013-02-19 17:31:33 -0800

    Use cloudfront to serve uploaded files.
    (As well as assets.)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,6 +8,7 @@ CarrierWave.configure do |config|
     :aws_access_key_id          => ENV['AWS_ACCESS_KEY_ID'],
     :aws_secret_access_key      => ENV['AWS_SECRET_ACCESS_KEY'],
   }
-  
-  config.fog_directory    = ENV['FOG_DIRECTORY']    # for cloudfront
+
+  config.fog_directory    = ENV['FOG_DIRECTORY']
+  config.asset_host       = ENV['CLOUDFRONT_DISTRIBUTION_DOMAIN']
 end
