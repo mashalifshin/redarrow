b282b0f157cab04da8db51575c5061dfa51872fe
Masha Lifshin <mlifshin@gmail.com>
2013-02-20 00:33:00 -0800

    Add sizing to slideshow image upload.

--- app/uploaders/slideshow_image_uploader.rb
+++ app/uploaders/slideshow_image_uploader.rb
@@ -1,4 +1,10 @@
-
 class SlideshowImageUploader < ImageUploader
+
+  include CarrierWave::MiniMagick
+
+  # Create different versions of your uploaded files:
+  version :slide do
+    process :resize_to_fit => [1300, 600]
+  end
 
 end
