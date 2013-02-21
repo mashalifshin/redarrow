8df993ef5b10eee69c10ae450b32a15810cfb9fa
Masha Lifshin <mlifshin@gmail.com>
2013-02-21 13:19:52 -0800

    Display and style about info images.

--- app/models/about_info.rb
+++ app/models/about_info.rb
@@ -1,6 +1,6 @@
 class AboutInfo < ActiveRecord::Base
   attr_accessible :body, :title, :live, :image, :image_cache, :remove_image
-  
+
   mount_uploader :image, AboutInfoImageUploader
 
   scope :live, :conditions => 'live IS TRUE'
