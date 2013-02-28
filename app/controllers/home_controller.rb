8b4e19cb2088d95aad1f4836f683ac2d8b406638
Masha Lifshin <mlifshin@gmail.com>
2013-02-27 21:28:57 -0800

    Don't shuffle here, do it in the slideshow js.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   protect_from_forgery  
   
   def show
-    @slideshow_images = SlideshowImage.live.shuffle
+    @slideshow_images = SlideshowImage.live
   end
 end
