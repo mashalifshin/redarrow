9376e0768dc66d5cc57abc934ef54512874ad18c
Masha Lifshin <mlifshin@gmail.com>
2013-02-27 21:16:50 -0800

    Return to using home/index instead of home/show.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     if user_signed_in?
-      redirect_to home_path, :alert => exception.message
+      redirect_to home_index_path, :alert => exception.message
     else
       redirect_to new_user_session_path, :alert => exception.message
     end
