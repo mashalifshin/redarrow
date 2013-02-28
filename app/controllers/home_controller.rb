5e9352661c1245b1f5900b763cb97b04ba298047
Masha Lifshin <mlifshin@gmail.com>
2013-02-27 21:24:56 -0800

    Keep homepage error free even without slides.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   protect_from_forgery  
   
   def show
-    @slideshow_images = SlideshowImage.live
+    @slideshow_images = SlideshowImage.live.shuffle
   end
 end
