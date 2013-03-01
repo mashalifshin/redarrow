388fcc58d3a5b371b85400b41e194fe4b08e1e74
Masha Lifshin <mlifshin@gmail.com>
2013-02-28 19:40:25 -0800

    Fix error that occurs if user has no name defined.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,6 @@ module ApplicationHelper
   end
 
   def users_first_name
-    current_user.name.split[0...1].first
+    current_user.name.split[0...1].first if user_signed_in? && !current_user.name.nil?
   end
 end
