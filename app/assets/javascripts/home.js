1eb074a42c923047ae186dc4048f340b8f78dc9e
Masha Lifshin <mlifshin@gmail.com>
2013-02-28 20:28:28 -0800

    User responsive slides for homepage slideshow.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,10 +1,5 @@
-//= require jquery.flexslider-min
-
 $(window).load(function(){
-	$('.flexslider').flexslider({
-		animation: "slide",
-		slideshowSpeed: 3000,
-		controlNav: false,
-		directionNav: false
+	$('.slides').responsiveSlides({
+		random: true
 	});
 });
